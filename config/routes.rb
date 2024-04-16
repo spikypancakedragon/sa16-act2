059e37e49b562cfb2214f16c09349bfe59de59e5
Avery Walker <spikypancakedragon@gmail.com>
2024-04-15 20:58:33 -0500

    added each page with very basic chat-gpt generated text, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
